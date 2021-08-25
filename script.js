9850c86ac0912fdb800f8eec9508f5657b132666
riessnico <nicolauriess@gmail.com>
2021-08-25 15:06:06 -0300

    added script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(3000, () => {
+	console.log("listening on 3000");
+});
