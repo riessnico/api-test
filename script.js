8e7716505228b10d0acd56d0e8b14dda2226bb49
riessnico <nicolauriess@gmail.com>
2021-08-25 15:17:26 -0300

    added initial sum route

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+const sumArray = (ar) => {
+	const sum = ar.reduce(reducer, 0);
+	if (sum) {
+		return sum;
+	}
+};
+
+app.post("/api/sum", (req, res) => {
+	const { array } = req.body;
+	const response = sumArray(array);
+	res.json({ response });
+});
+
 app.listen(3000, () => {
 	console.log("listening on 3000");
 });
